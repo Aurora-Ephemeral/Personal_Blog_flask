f348187fc6ff1ba55156c4ffcfa30e71f8ed0004
FIXED-TERM Lu Xu (CR/PJ-AI-E) <lux1rng@bosch.com>
2022-02-01 09:36:14 +0100

    add new feature: time order and user specific page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,6 @@
 import secrets 
 import os 
+from datetime import datetime
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
@@ -12,7 +13,8 @@ profile_root_path = os.path.join(app.root_path, 'static/profile_img')
 @app.route('/')
 @app.route('/home')
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts, title="home")
 
 @app.route('/about')
@@ -122,6 +124,7 @@ def update_post(post_id):
     if form.validate_on_submit(): 
         post.title = form.title.data
         post.content = form.content.data
+        post.date_posted = datetime.utcnow()
         db.session.commit()
         flash('Your Psot has been updated', 'success')
         return redirect(url_for('post', post_id=post.id))
@@ -129,6 +132,21 @@ def update_post(post_id):
         form.title.data = post.title 
         form.content.data = post.content 
     return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
-@app.route('/post/<int:post_id>/delete')
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
 def delete_post(post_id):
-    pass
+    post = Post.query.get_or_404(post_id)
+    form = PostForm()
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your Post has been deleted', 'success')
+    return redirect(url_for('home'))
+
+@app.route('/home/<string:username>')
+def user_post(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
